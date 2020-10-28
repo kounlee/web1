6667b0de5d12745f3f001b3e1b3d593d5456d86e
kounlee <73319020+kounlee@users.noreply.github.com>
2020-10-28 09:21:58 +0900

    화요일

--- run.py
+++ run.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_pymongo import pymongo
 
 app = Flask(__name__)
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/koun_project"
+#app.config["SECRET_KEY"] = "abcd"
+#mongo = PyMongo(app)
 
-
-@app.route("/")
+@app.route("/",methods=["GET"])
 def index():
-    return "안녕"
+    return render_template("index.html")
 
 
-if __name__=="__main":
-    app.run(debug=True)
+@app.route("/maker",methods=["GET"])
+def maker():
+    return render_template("maker.html")
+
+
+@app.route("/notice",methods=["GET"])
+def notice():
+    return render_template("notice.html")
+
+
+@app.route("/problem",methods=["GET"])
+def problem():
+    if request.method == 'GET' :
+        name = request.args.get()
+        print()
+    return render_template("problem.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True, port=9000)
